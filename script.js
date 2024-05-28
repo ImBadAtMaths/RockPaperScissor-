d5f1cdcaacf3f023cd3ec06560c42c3d54c3bad6
Anujay Singh <109102210+ImBadAtMaths@users.noreply.github.com>
2024-05-28 17:33:08 +0530

    Fixed a bug!

--- script.js
+++ script.js
@@ -8,19 +8,24 @@ let endGame = document.getElementById("end");
 // Variables For Points 👇
 let computerPtn = document.getElementById("computerPtn");
 let userPtn = document.getElementById("userPtn");
+let computer_points = 0;
+let user_points = 0;
 
 // Variables for DOM 👇
 let random_Guess_hai = document.getElementById("random_Guess_hai");
 let result = document.getElementById("result");
 let user_guess_hai = document.getElementById("user_guess_hai");
 
+// StartGame Function 👇
 startGame.addEventListener("click", () => {
+  
+  // Variable Declaration 👇
   let randomGuess;
   let user_guess;
-  let randomGuessArray = ["rock", "paper", "scissor"];
   let computerGuess;
-  let computer_points = 0;
-  let user_points = 0;
+
+  // This helps the randomGuess to randomize the strings using computerGuess 👇
+  let randomGuessArray = ["rock", "paper", "scissor"];
 
   rock.addEventListener("click", () => {
     randomGuess = Math.floor(Math.random() * 3);
@@ -28,16 +33,17 @@ startGame.addEventListener("click", () => {
     random_Guess_hai.innerText = `Computer Guess: ${computerGuess}`;
     user_guess = "rock";
     user_guess_hai.innerText = `User Guess: ${user_guess}`;
+
     if (user_guess && computerGuess == "rock") {
       result.innerText = "It's a Draw";
     } else if (user_guess && computerGuess == "paper") {
       result.innerText = "Computer Wins";
       computer_points++;
-      computerPtn.innerText = `Computer Points: ${computer_points}`
+      computerPtn.innerText = `Computer Points: ${computer_points}`;
     } else if (user_guess && computerGuess == "scissor") {
       result.innerText = "User Wins";
       user_points++;
-      userPtn.innerText = `User Points: ${user_points}`
+      userPtn.innerText = `User Points: ${user_points}`;
     }
   });
 
@@ -50,13 +56,13 @@ startGame.addEventListener("click", () => {
     if (user_guess && computerGuess == "rock") {
       result.innerText = "User Wins";
       user_points++;
-      userPtn.innerText = `User Points: ${user_points}`
+      userPtn.innerText = `User Points: ${user_points}`;
     } else if (user_guess && computerGuess == "paper") {
       result.innerText = "It's a draw";
     } else if (user_guess && computerGuess == "scissor") {
       result.innerText = "Computer Wins";
       computer_points++;
-      computerPtn.innerText = `Computer Points: ${computer_points}`
+      computerPtn.innerText = `Computer Points: ${computer_points}`;
     }
   });
 
@@ -69,12 +75,11 @@ startGame.addEventListener("click", () => {
     if (user_guess && computerGuess == "rock") {
       result.innerText = "Computer wins";
       computer_points++;
-      computerPtn.innerText = `Computer Points: ${computer_points}`
+      computerPtn.innerText = `Computer Points: ${computer_points}`;
     } else if (user_guess && computerGuess == "paper") {
       result.innerText = "User Wins";
       user_points++;
-      userPtn.innerText = `User Points: ${user_points}`
-
+      userPtn.innerText = `User Points: ${user_points}`;
     } else if (user_guess && computerGuess == "scissor") {
       result.innerText = "Draw";
     }
